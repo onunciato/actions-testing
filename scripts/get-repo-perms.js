aa751d35657d8a7faf5a096fbc6c3c24ac9cdd85
Christian Nunciato <c@nunciato.org>
2022-07-29 16:07:02 -0700

    Update get-repo-perms.js

--- scripts/get-repo-perms.js
+++ scripts/get-repo-perms.js
@@ -5,6 +5,8 @@ const octokit = require("@octokit/rest");
     const [ owner, repo ] = process.env.GITHUB_REPOSITORY.split("/");
     const username = process.env.GITHUB_ACTOR;
     const client = new octokit.Octokit({ auth });
+    
+    console.log({ owner, repo, username });
 
     try {
         const result = await client.repos.getCollaboratorPermissionLevel({ owner, repo, username });
